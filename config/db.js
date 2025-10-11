a9a31671fdf7f4933420ac0f86a7890c6d9172ed
LerdoContemporaneo <lerdoappcontemporaneo@gmail.com>
2025-10-10 19:50:14 -0600

    mysql2

--- config/db.js
+++ config/db.js
@@ -1,13 +1,19 @@
 import { Sequelize } from "sequelize";
-import mysql2 from "mysql2";
+import mysql2 from "mysql2"; // 👈 Importación del módulo real
 import dotenv from "dotenv";
+
 dotenv.config();
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
     host: process.env.DB_HOST,
-    dialect: 'mysql',
-    dialectModule: 'mysql2',
+    dialect: "mysql",
+    dialectModule: mysql2, // 👈 Aquí va el módulo importado, no una cadena
     logging: false,
-});
+  }
+);
 
 export default db;
